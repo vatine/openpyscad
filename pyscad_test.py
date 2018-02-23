148574ac700bc4d2d978c98986c3aeeaf127ff9f
Ingvar Mattsson <imagineaclevernamehere@gmail.com>
2018-02-23 09:29:59 +0000

    Some more tests added, some changed to match actual expected output.

--- pyscad_test.py
+++ pyscad_test.py
@@ -3,31 +3,7 @@ import StringIO
 
 import pyscad
 
-class SphereTest(unittest.TestCase):
 
-    def testNoParameters(self):
-        self.assertRaises(pyscad.ParameterError, pyscad.Sphere)
-
-    def testTwoParameters(self):
-        self.assertRaises(pyscad.ParameterError, pyscad.Sphere, 1, 1)
-
-    def testSpecifyRadius(self):
-        s = pyscad.Sphere(radius=1.0)
-        self.assertEqual(1.0, s.radius)
-
-    def testSpecifyDiameter(self):
-        s = pyscad.Sphere(diameter=2.0)
-        self.assertEqual(1.0, s.radius)
-
-    def testRender(self):
-        dump = StringIO.StringIO()
-        s = pyscad.Sphere(diameter=2.0)
-        s.render(dump)
-        seen = dump.getvalue()
-        expected = 'sphere(r = 1.000000)\n'
-        self.assertEqual(seen, expected)
-
-        
 class BoxTest(unittest.TestCase):
     def testNoParameters(self):
         self.assertRaises(pyscad.ParameterError, pyscad.Box)
@@ -66,9 +42,61 @@ class BoxTest(unittest.TestCase):
         b = pyscad.Box(width=1.0, depth=4.0, height=9.0)
         b.render(dump)
         seen = dump.getvalue()
-        expected = 'cube(size = [1.000000, 4.000000, 9.000000], center = false)\n'
+        expected = 'cube(size = [1.000000, 4.000000, 9.000000], center = false);\n'
         self.assertEqual(seen, expected)
 
 
+class SphereTest(unittest.TestCase):
+    def testNoParameters(self):
+        self.assertRaises(pyscad.ParameterError, pyscad.Sphere)
+
+    def testAllParameters(self):
+        self.assertRaises(pyscad.ParameterError, pyscad.Sphere,
+                          radius=1.0, diameter=2.0)
+
+    def testPassRadius(self):
+        s = pyscad.Sphere(radius=1.0)
+        self.assertEqual(s.radius, 1.0)
+
+    def testPassDiameter(self):
+        s = pyscad.Sphere(diameter=2.0)
+        self.assertEqual(s.radius, 1.0)
+
+    def testRender(self):
+        dump = StringIO.StringIO()
+        s = pyscad.Sphere(radius=1.0)
+        s.render(dump)
+        seen = dump.getvalue()
+        expected = 'sphere(r = 1.000000);\n'
+        self.assertEqual(seen, expected)
+
+
+class UnionTest(unittest.TestCase):
+
+    def testUnionNoObjects(self):
+        dump = StringIO.StringIO()
+        u = pyscad.Union()
+        u.render(dump)
+        expected = 'union() {\n}\n'
+        seen = dump.getvalue()
+        self.assertEqual(seen, expected)
+
+    def testSingleObject(self):
+        dump = StringIO.StringIO()
+        u = pyscad.Union(pyscad.Sphere(radius=1.0))
+        u.render(dump)
+        expected = 'union() {\n  sphere(r = 1.000000);\n}\n'
+        seen = dump.getvalue()
+        self.assertEqual(seen, expected)
+        
+    def testMultipleObjects(self):
+        dump = StringIO.StringIO()
+        u = pyscad.Union(pyscad.Sphere(radius=1.0), pyscad.Sphere(radius=2.0))
+        u.render(dump)
+        expected = 'union() {\n  sphere(r = 1.000000);\n  sphere(r = 2.000000);\n}\n'
+        seen = dump.getvalue()
+        self.assertEqual(seen, expected)
+
+                
 if __name__ == '__main__':
     unittest.main()
